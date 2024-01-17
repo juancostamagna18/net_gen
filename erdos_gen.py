4edcf1fd2d8ef6079112003d2c2d005d7f39bae4
Juan Costamagna <juancostamagna1986@gmail.com>
2024-01-17 00:42:40 -0300

    Update erdos_gen.py

--- erdos_gen.py
+++ erdos_gen.py
@@ -6,35 +6,35 @@ import os, psutil
 from time import time
 from utilities import *
 
-def erdos(stars, p):
-  st = time()
-  solution_net = []
-  
-  am_nodes.append(amount_stars)
-  for i in range(amount_stars-1):
-    for j in range(i+1, amount_stars):
-      chance = np.random.rand()
-      #p = dist1(i,j,stars_fil)/12.1
-      #if p > 1:
-       # p = 0 # el camino es mas grande en distancia que el total a Trass
-      if p > chance:
-        solution_net.append((i,j))
+def erdos_gen(stars,p, amount_stars,edge_lenght, max_lenght=False):
+    '''
+    creates a net with the Erdos model.
+    Arguments:
+        stars: a list of dictionaries with the stars data
+        p: the probability of disconect an edge(optional float in case have max_lenght = true)
+        amount_stars: number of stars for the net (integer)
+        edge_lenght: maximum allowable lenght for every edge in the net
+        max_lenght: tell us if are usign maximum allowable lenght in the algorithm
+    Returns:
+        The net in a list of edges, the elapsed time, the memory used by the algorithm.
+    '''
+    edges_act = []
+    st = time()
+    for i in range(amount_stars-1):
+        for j in range(i+1, amount_stars):
+            chance = np.random.rand()
+            if max_lenght:
+                p = dist1(i,j,stars)/edge_lenght
+                if p > 1:
+                    p = 0 # el camino es mas grande en distancia que el total a Trass
+            if p > chance:
+                edges_act.append((i,j))
+    end_t = time()
+    elapsed_time = end_t - st
+    process = psutil.Process(os.getpid())
+    mem_us = process.memory_info().rss/1024**2
 
-  process = psutil.Process(os.getpid())
-  if (0,amount_stars-1) in solution_net:
-    solution_net.remove((0,amount_stars-1))
-  if not (0,amount_stars-1) in solution_net:
-    print("no esta")
-
-  end_t = time()
-  elapsed_time = end_t - st
-  process = psutil.Process(os.getpid())
-  mem_usage = process.memory_info().rss/1024**2
-  
-  
-  return solution_net, elapsed_time, mem_usage
-
-
+    return edges_act, elapsed_time, mem_us
 
 am_nodes = []
 time_list = []
@@ -45,7 +45,8 @@ p = 0.8
 
 for amount_stars in range(10,101,10):
   stars = import_database_ran('./base_final.csv',100)
-  solution_net, elapsed_time, mem = erdos(stars,p)
+  am_nodes.append(amount_stars)
+  solution_net, elapsed_time, mem = erdos_gen(stars,p,amount_stars,12.1)
   time_list.append(elapsed_time)
   mem_us.append(mem)
   net_plot(stars,amount_stars,solution_net)
