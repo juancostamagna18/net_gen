ed9b237925bbc9995577b33b4018d1e463bf244a
Juan Costamagna <juancostamagna1986@gmail.com>
2024-01-17 01:10:44 -0300

    function random_gen

--- random_gen.py
+++ random_gen.py
@@ -8,18 +8,9 @@ from functools import reduce
 from operator import and_
 from utilities import *
 
-d_list = []
-mem_us = []
-time_list = []
-am_nodes = []
-
-
-for amount_stars in range(10,101,10):
-  selected_arcs = []
+def random_gen(stars):
   all_connected = False
-  stars = import_database_ran('./base_final.csv', amount_stars)
-  tras_index, stars_fil = add_tras(stars,amount_stars)
-
+  
   G1 = Graph(directed=False)
   G1.add_vertex(amount_stars)
   edge_weight = G1.new_edge_property("double")
@@ -53,6 +44,19 @@ for amount_stars in range(10,101,10):
   end_time = time()
   elapsed_time = end_time - st_time
   mem = process.memory_info().rss/1024**2
+  return selected_arcs, elapsed_time, mem
+
+d_list = []
+mem_us = []
+time_list = []
+am_nodes = []
+
+
+for amount_stars in range(10,101,10):
+  selected_arcs = []
+  stars = import_database_ran('./base_final.csv', amount_stars)
+  tras_index, stars_fil = add_tras(stars,amount_stars)
+  selected_arcs, elapsed_time, mem = random_gen(stars_fil)
   #dist = shortest_distance(G1,0,tras_index,weights=edge_weight)
   dist, path = calculate_s_path(stars_fil,amount_stars,selected_arcs,tras_index)
   d_list.append(dist)
